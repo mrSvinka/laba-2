0f2cf725c30a699fc3e80d86240d1e4afbdc9d39
mrSvinka <181984796+mrSvinka@users.noreply.github.com>
2024-10-05 13:00:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,28 +15,35 @@ using namespace std;
         S - площадь
                          S = (a * b * sinα) / 2 - фрмуля
                         треугольник заведомо существует*
-
 */
 
+double calculateArea(double A, double B, double radians) //формула.
+{
+    double area = A * B * sin(radians)*0.5;
+    return area;
+}
 
-
-double degrees_to_radians(double degrees)   //приобразует градусы в радианы
+double degrees_to_radians(double degrees)   //приобразует градусы в радианы.
 {
     return degrees * M_PI / 180.0;
 }
 
-int main()  //созд. перемен. Получает и сохран. данные
+int main()  //созд. перемен. Получает и сохран. данные.
 {
     double A;
+    std::cout << "side A: ";
     std::cin >> A;
 
     double B;
+    std::cout << "side B: ";
     std::cin >> B;
 
     double degrees;
+    std::cout << "corner: ";
     std::cin >> degrees;
-    double radians = degrees_to_radians(degrees); //радианы
-
-
-
+    double radians = degrees_to_radians(degrees); //радианы.
+    
+    std::cout << "Square: " << calculateArea(A, B, radians) << std::endl; //вывод данных.
+    
+    return 0;
 }
